b8a6ff19dba494e0b3a7905aef4bf57a63490ef5
Dinar <dinar-wp9@mail.ru>
2023-06-26 17:36:10 +0300

    reorder files

---
+++ LinkStorage.Tests/API/Controllers/TestUserAuthController.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using LinkStorage.Controllers;
+using LinkStorage.DTO;
+using LinkStorage.Repository.IRepository;
+using LinkStorage.Tests.MockData;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkStorage.Tests.API.Controllers
+{
+    public class TestUserAuthController
+    {
+        private LoginRequestDTO model;
+        private LoginRequestDTO loginRequestDTO;
+
+        [Fact]
+        public async Task GetAllAsync_ShouldReturn200Status()
+        {
+            ///Arrange
+            var _userRepos = new Mock<IUserRepository>();
+            _userRepos.Setup(x => x.Login(loginRequestDTO)).ReturnsAsync(UserMockData.LoginResponce);
+
+            var sut = new UserAuthController(_userRepos.Object);
+            ///act
+            var result = (OkObjectResult)await sut.Login(model);
+
+            ///Assert
+            result.StatusCode.Should().Be(200);
+            _userRepos.Invocations.Should().HaveCount(1);
+        }
+    }
+}
---
+++ LinkStorage.Tests/MockData/UserMockData.cs
@@ -0,0 +1,23 @@
+﻿using LinkStorage.DTO;
+using LinkStorage.Models;
+
+
+namespace LinkStorage.Tests.MockData
+{
+    public class UserMockData
+    {
+        public static LoginResponceDTO LoginResponce { get; set; } = new LoginResponceDTO
+        {
+            User = new User()
+            {
+                Id = 1,
+                Name = "Test1",
+                Surname = "Testov1",
+                Role = "user",
+                Email = "thisEmail@a.com",
+                Password = "password"
+            },
+            Token = "asdasdasd"
+        };
+    }
+}
